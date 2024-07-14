2a25c3a36cfb902809f923ab0c525e40f2996b0a
Dmytro Skintiian <dmytro.skintiian@gmail.com>
2024-07-14 19:14:13 +0300

    Завдання 1

---
+++ 01_salary/salary_calculator.py
@@ -0,0 +1,37 @@
+def total_salary(path: str) -> list or None:
+    salaries = []
+    try:
+        with open(path, encoding='utf-8') as file:
+            for line in file:
+                _, salary = line.strip().split(',')
+
+                if not salary.isdigit():
+                    raise ValueError(f'Файл містить невалідні дані. [Рядок "{line.strip()}"]')
+
+                salaries.append(int(salary))
+
+            return [sum(salaries), sum(salaries) / len(salaries)]
+    except FileNotFoundError:
+        print(f'Файл не знайдено [Шлях "{path}"]')
+    except ValueError as error:
+        print(error)
+    except ZeroDivisionError:
+        print(f'Файл порожній [Шлях "{path}"]')
+
+    return None
+
+
+
+if __name__ == '__main__':
+    # correct
+    total, average = total_salary('./salaries.txt')
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average:.0f}")
+
+    # file not found
+    total_salary('./not_found_salaries.txt')
+
+    # invalid data in file
+    total_salary('./salaries_invalid.txt')
+
+    # empty file
+    total_salary('./salaries_empty.txt')
